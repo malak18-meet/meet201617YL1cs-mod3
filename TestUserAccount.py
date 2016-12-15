660c5a0f22d3aac003a2a332360549dc3640b4c9
malak18-meet <malak.jabarin@gmail.com>
2016-12-15 18:09:58 +0200

    made tests for UserAccount class

---
+++ TestUserAccount.py
@@ -0,0 +1,12 @@
+#This script performs some simple tests on the UserAccount class.
+from UserAccount import UserAccount
+
+#Three things are missing from the line below - fill them in
+my_user=UserAccount("malak","malak12","i_love_shira")
+
+#Call the print_secret method (function) - it takes one input - a guess for the password.
+#Use the wrong password as input here
+my_user.print_secret("12345")
+
+#Use the right password here
+my_user.print_secret("malak12")
